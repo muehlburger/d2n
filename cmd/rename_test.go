228bc61765d7d1f32cc81bcce574cea7d6b0d80f
Herbert Mühlburger <mail@muehlburger.at>
2019-08-25 23:28:20 +0200

    update unit tests

--- cmd/rename_test.go
+++ cmd/rename_test.go
@@ -10,8 +10,6 @@ import (
 
 // setup creates a testfile and sets creation and modification times.
 func setup(t *testing.T) (*os.File, func()) {
-	t.Parallel()
-
 	mTime := time.Date(2016, time.February, 20, 5, 52, 3, 0, time.Local)
 	aTime := time.Date(2016, time.February, 20, 5, 52, 0, 0, time.Local)
 
@@ -28,15 +26,25 @@ func setup(t *testing.T) (*os.File, func()) {
 	return f, teardown
 }
 
-func TestRename(t *testing.T) {
+func TestRenameToModTime(t *testing.T) {
 	f, teardown := setup(t)
 	defer teardown()
 
-	n := Rename(f.Name())
+	n := filepath.Base(Rename(f.Name()))
 
-	got := filepath.Base(n)
-	exp := "2016-02-20T05.52.03"
-	if exp != got {
-		t.Fatalf("wanted: %v; got: %v", exp, got)
+	if n != "2016-02-20T05.52.03" {
+		t.Errorf("filename should be \"2016-02-20T05.52.03\"; got: %v", n)
+	}
+
+	n = filepath.Base(Rename(f.Name()))
+
+	if n != "2016-02-20T05.52.03" {
+		t.Errorf("filename should be \"2016-02-20T05.52.03\"; got: %v", n)
+	}
+
+	n = filepath.Base(Rename(f.Name()))
+
+	if n != "2016-02-20T05.52.03" {
+		t.Errorf("filename should be \"2016-02-20T05.52.03\"; got: %v", n)
 	}
 }
